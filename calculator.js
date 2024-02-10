099dc043eebfdee2e0e22b3e5bad459685dbc39c
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-10 18:12:34 -0500

    Refactor state management as JS object

--- calculator.js
+++ calculator.js
@@ -6,25 +6,27 @@
 import { operate } from './operations.js'
 
 
-let displayValue = '';
-let firstValue = '';
-let secondValue = '';
-let operator = '';
-let operatorClicked = false;
+const state = {
+    display: '',
+    first: '',
+    second: '',
+    operator: '',
+    operatorClicked: false
+};
 
 
 const display = document.querySelector('.display-content');
 const digitButtons = document.querySelectorAll('.digit');
 digitButtons.forEach(button => {
     button.addEventListener('click', function() {
-        if (operatorClicked)
+        if (state.operatorClicked)
         {
-            displayValue = '';
-            operatorClicked = false;
+            state.display = '';
+            state.operatorClicked = false;
         }
 
-        displayValue += this.value;
-        display.textContent = displayValue;
+        state.display += this.value;
+        display.textContent = state.display;
     });
 });
 
@@ -32,30 +34,30 @@ digitButtons.forEach(button => {
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => {
     button.addEventListener('click', function() {
-        firstValue = parseInt(displayValue);
-        operator = button.value;
-        operatorClicked = true;
+        state.first = parseInt(state.display);
+        state.operator = button.value;
+        state.operatorClicked = true;
     });
 });
 
 
 const equalsButton = document.querySelector('.equals');
 equalsButton.addEventListener('click', function() {
-    if (firstValue && !operatorClicked)
+    if (state.first && !state.operatorClicked)
     {
-        secondValue = parseInt(displayValue);
-        displayValue = operate(firstValue, secondValue, operator);
-        display.textContent = displayValue;
-        operatorClicked = true;
+        state.second = parseInt(state.display);
+        state.display = operate(state.first, state.second, state.operator);
+        display.textContent = state.display;
+        state.operatorClicked = true;
     }
 });
 
 
 const clearButton = document.querySelector('.func-clear');
 clearButton.addEventListener('click', function() {
-    firstValue = secondValue = '';
-    displayValue = 0;
-    operatorClicked = false;
+    state.first = state.second = '';
+    state.display = 0;
+    state.operatorClicked = false;
 
-    display.textContent = displayValue;
+    display.textContent = state.display;
 });
