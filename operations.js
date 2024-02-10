0d697e1faf85af19d4c198725685410f9e6e9efa
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-10 18:07:28 -0500

    Refactor JS code into two script files

---
+++ operations.js
@@ -0,0 +1,31 @@
+// Daniel Fletcher
+// The Odin Project: Foundations
+// Calculator
+
+
+// Required mathematical operations
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+
+// Parse and perform specified operation
+function operate(num1, num2, operator) {
+    switch (operator)
+    {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return undefined;
+    }
+}
+
+
+export { operate };
