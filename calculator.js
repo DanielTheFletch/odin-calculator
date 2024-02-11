ae91d5e26b15d281b6dcc0cd291b880af6faef07
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-10 19:56:23 -0500

    Polish calculator logic and behavior

--- calculator.js
+++ calculator.js
@@ -7,29 +7,57 @@
 import { operate } from './operations.js'
 
 
-// State management
+// State management: Variables
 const state = {
     display: '',
     first: '',
     second: '',
     operator: '',
-    operatorClicked: false
 };
 
 
+// State management: Functions
+const updateFirst = () => state.first = parseInt(state.display);
+const updateSecond = () => state.second = parseInt(state.display);
+const clearDisplay = () => state.display = '';
+
+
+// Status of current operation
+const operation = {
+    complete: false,
+    inProgress: false,
+    selected: false
+};
+
+
+
+// Update the display area on the DOM Tree
+const displayArea = document.querySelector('.display-content');
+const updateDisplayArea = () => displayArea.textContent = state.display;
+
+
+// Compute the currently in-progress operation
+function compute() {
+    state.display = operate(state.first, state.second, state.operator);
+    updateDisplayArea();
+}
+
+
 // Set up 0-9 digit buttons
-const display = document.querySelector('.display-content');
 const digitButtons = document.querySelectorAll('.digit');
 digitButtons.forEach(button => {
     button.addEventListener('click', function() {
-        if (state.operatorClicked)
+        // Reset display on new operand
+        if (operation.selected || operation.complete)
         {
-            state.display = '';
-            state.operatorClicked = false;
+            clearDisplay();
+            operation.selected = false;
+            operation.complete = false;
         }
 
+        // Track and display current operand
         state.display += this.value;
-        display.textContent = state.display;
+        updateDisplayArea();
     });
 });
 
@@ -38,9 +66,22 @@ digitButtons.forEach(button => {
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => {
     button.addEventListener('click', function() {
-        state.first = parseInt(state.display);
-        state.operator = button.value;
-        state.operatorClicked = true;
+        if (!operation.selected)
+        {
+            // Allow for chaining multiple operations together
+            if (operation.inProgress)
+            {
+                // Perform intermediary calculation
+                updateSecond();
+                compute();
+            }
+            
+            // Update values for mid-operation status
+            updateFirst();
+            operation.inProgress = true;
+            operation.selected = true;
+            state.operator = this.value;
+        }
     });
 });
 
@@ -48,12 +89,15 @@ operatorButtons.forEach(button => {
 // Set up equals (=) function
 const equalsButton = document.querySelector('.equals');
 equalsButton.addEventListener('click', function() {
-    if (state.first && !state.operatorClicked)
+    if (operation.inProgress && !operation.selected)
     {
-        state.second = parseInt(state.display);
-        state.display = operate(state.first, state.second, state.operator);
-        display.textContent = state.display;
-        state.operatorClicked = true;
+        // Perform final calculation
+        updateSecond();
+        compute();
+
+        // Update values for end-of-operation status
+        operation.inProgress = false;
+        operation.complete = true;
     }
 });
 
@@ -61,9 +105,9 @@ equalsButton.addEventListener('click', function() {
 // Set up clear (AC) function
 const clearButton = document.querySelector('.func-clear');
 clearButton.addEventListener('click', function() {
-    state.first = state.second = '';
-    state.display = 0;
-    state.operatorClicked = false;
-
-    display.textContent = state.display;
+    state.first = '';
+    state.second = '';
+    clearDisplay();
+    state.operator = '';
+    displayArea.textContent = '0';
 });
