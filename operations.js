fa83200fde260e7210a51e823f0db60b227a5a1b
Daniel Fletcher <90067982+DanielTheFletch@users.noreply.github.com>
2024-02-11 18:05:45 -0500

    Disallow division by 0

--- operations.js
+++ operations.js
@@ -7,7 +7,7 @@
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const divide = (num1, num2) => ((num2 === 0) ? 'ERROR' : (num1 / num2));
 
 
 // Parse and perform specified operation
